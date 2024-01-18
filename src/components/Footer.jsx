116eaff7f6eb1abb9a0ae977ee8338488ec703b8
chisom chima <chimachisom360@gmail.com>
2024-01-18 16:14:44 +0100

    done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -30,10 +30,10 @@ const FooterPage = () =>  (
 
       </div> */}
       <div className='w-full flex justify-between sm:flex-row flex-col border-t-[1px] border-t-[#3f3e45] items-center pt-6 '>
-      <img src={ZumaLogo} alt="logo" className='w-[100px] h-[100px] object-contain rounded-full' />
+      {/* <img src={ZumaLogo} alt="logo" className='w-[100px] h-[100px] object-contain rounded-full' /> */}
         <p className='font-poppins text-white text-[18px] leading-[27px] text- normal text-center'>2023 Zuma Consults All Right Reserved.</p>
         <div className='flex flex-row md:mt-0 mt-6'>
-          <div className='text-white'> Email: <span className='text-white font-bold'> Info@zumaconsults.com</span></div>
+          <div className='text-white mr-2'> Email: <span className='text-white font-bold'> Info@zumaconsults.com</span></div>
           {socialMedia.map((social,index)=>(
            <a href={social.link} target='_blank'><img key={social.id} src={social.icon} alt={social.id} className={`w-[22px] h-[21] object-contain cursor-pointer mx-2`} /></a>
           ))}
