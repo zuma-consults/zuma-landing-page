3dcdceb4592e82115d4e754be29bede9ceb4a0ce
Chisomchima <chimachisom360@gmail.com>
2024-08-23 10:12:12 +0100

    fix favicon

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import styles from '../style'
 import { ZumaLogo } from '../assets'
-import { footerLinks, socialMedia } from '../constants'
+import { socialMedia } from '../constants'
 
 const FooterPage = () =>  (
     <section className={`${styles.flexCenter} ${styles.paddingY} flex-col`}>
@@ -9,7 +9,7 @@ const FooterPage = () =>  (
       <img src={ZumaLogo} alt="logo" className='w-[70px] h-[70px] object-contain' />
         <p className='font-poppins text-white text-[18px] leading-[27px] text- normal text-center'>Okike Consults Limited All Right Reserved.</p>
         <div className='flex flex-row md:mt-0 mt-6'>
-          <div className='text-white mr-2'> Email: <span className='text-white font-bold'> Info@zumaconsults.com</span></div>
+          <div className='text-white mr-2'> Email: <span className='text-white font-bold'>okikeconsults@gmail.com</span></div>
           {socialMedia.map((social,index)=>(
            <a href={social.link} target='_blank'><img key={social.id} src={social.icon} alt={social.id} className={`w-[22px] h-[21] object-contain cursor-pointer mx-2`} /></a>
           ))}
--- src/constants/index.js
+++ src/constants/index.js
@@ -176,11 +176,11 @@ export const footerLinks = [
 ];
 
 export const socialMedia = [
-  // {
-  //   id: "social-media-1",
-  //   icon: instagram,
-  //   link: "https://www.instagram.com/",
-  // },
+  {
+    id: "social-media-1",
+    icon: instagram,
+    link: "https://www.instagram.com/okikeconsults/",
+  },
   // {
   //   id: "social-media-2",
   //   icon: facebook,
@@ -194,7 +194,7 @@ export const socialMedia = [
   {
     id: "social-media-4",
     icon: linkedin,
-    link: "https://www.linkedin.com/company/zuma-consults/",
+    link: "https://www.linkedin.com/company/okike-consults/",
   },
 ];
 
